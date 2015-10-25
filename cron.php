a1b4f1ae05b01020ccd508603f60981291ac1f26
Stawen DRONEK <stanislas.draunet@gmail.com>
2015-10-25 21:13:40 +0000

    fix fuseau horaire dans higtchart, tout semble venir de la les probs de dates...

--- cron.php
+++ cron.php
@@ -9,12 +9,12 @@ include_once('config.php');
 
 
 $oko = new okofen();
-$oko2web = new oko2ftp();
+//$oko2web = new oko2ftp();
 
 //on telecharge le csv depuis la chaudiere
 if(GET_CHAUDIERE_DATA_BY_IP) $oko->getChaudiereData();
 //envoi du fichier vers le serveur web distant
-if(SEND_TO_WEB) $oko2web->send2web();
+//if(SEND_TO_WEB) $oko2web->send2web();
 //integre le csv dans la base
 $oko->csv2bdd();
 //on lance le traitement
