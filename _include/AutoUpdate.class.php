b39e498bd1651be84cec89703a188ee1e2482cd8
Stawen DRONEK <stanislas.draunet@gmail.com>
2015-11-02 12:51:57 +0000

    roolback

--- _include/AutoUpdate.class.php
+++ _include/AutoUpdate.class.php
@@ -546,9 +546,6 @@ class AutoUpdate extends connectDb{
 			
 			$filename = str_replace($gitFolder,"",zip_entry_name($file));
 			//$filename = zip_entry_name($file);
-			
-			if($filename == null) continue;
-			
 			$foldername = $this->_installDir . dirname($filename);
 			$absoluteFilename = $this->_installDir . $filename;
 
