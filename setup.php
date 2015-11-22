d4a9dd615e25c51feeb41414553241cbccadef79
Stawen DRONEK <stanislas.draunet@gmail.com>
2015-11-22 10:08:56 +0000

    fix #51 - failed setup

--- setup.php
+++ setup.php
@@ -128,9 +128,33 @@
 	
 ?>
 
-<?php
-	include('_templates/header.php');
-?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>OkoVision</title>
+    
+	<!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/jquery-ui.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>+
+    <![endif]-->
+	<?php //include_once("analyticstracking.php"); ?>
+	
+	</head>
+
+  <body role="document">
+  	
 <div class="container theme-showcase" role="main">
 		<div class="page-header" align="center">
 			<h2>Installation Okovision</h2> <br>
