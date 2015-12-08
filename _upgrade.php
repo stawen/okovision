388b45a9d3376cf9d42f600e746d290cdf3e6d1b
Stawen DRONEK <stanislas.draunet@gmail.com>
2015-12-08 09:55:08 +0000

    remove upgrade 1.6.3

--- _upgrade.php
+++ _upgrade.php
@@ -6,15 +6,5 @@ ini_set('max_execution_time', 600);
 $this->log->info("UPGRADE | $version | begin");
 $t = new timeExec();
 
-$configFile = file_get_contents('config.php');
-$configFile = str_replace("DEFINE('BDD_DECIMAL','.');","DEFINE('BDD_DECIMAL','.');\n//UNIQUE TOKEN ID\nDEFINE('TOKEN','###_TOKEN_###');\n//NEWPARAMUPDATE",$configFile);
-
-$token = sha1(rand());
-$configFile = str_replace("###_TOKEN_###",$token,$configFile);
-
-file_put_contents('config.php',$configFile);
-
-
-
 $this->log->info("UPGRADE | $version | end :".$t->getTime());
 ?>
