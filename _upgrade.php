f7e1675a52d566b5614b0e919ee1d47a0e06e579
Stawen DRONEK <stanislas.draunet@gmail.com>
2017-02-01 21:29:06 +0000

    fix update for 1.8.1

--- _upgrade.php
+++ _upgrade.php
@@ -3,6 +3,13 @@
 ini_set('max_execution_time', 600);
 
 $this->log->info("UPGRADE | $version | begin");
+$t = new timeExec();
+
+/*
+* ici le code pour un upgrade
+*/
+
+
 $this->log->info("UPGRADE | $version | end :".$t->getTime());
 
 ?>
